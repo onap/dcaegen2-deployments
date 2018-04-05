1e37279472cb57e1013013191b50146c9bed408d
Lusheng Ji <lji@research.att.com>
2018-04-04 21:51:34 -0400

    Fix TCA image
    Issue-ID: DCAEGEN2-206
    Change-Id: I8fa5150bffc0e371ab09bd75f1df4e460bf85a72
    Signed-off-by: Lusheng Ji <lji@research.att.com>

--- tca-cdap-container/get-tca.sh
+++ tca-cdap-container/get-tca.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+# ================================================================================
+# Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+#
+
+ARTIFACTPATH=${1:-/opt/tca/}
 PROTO='https'
 NEXUSREPO='nexus.onap.org'
 REPO='snapshots'
 GROUPID='org.onap.dcaegen2.analytics.tca'
-ARTIFACTID='dcae-analytics-tca'
+ARTIFACTID='dcae-analytics-cdap-tca'
 VERSION='2.2.0-SNAPSHOT'
 
 URL="${PROTO}://${NEXUSREPO}/service/local/repositories/${REPO}/content/${GROUPID//.//}/${ARTIFACTID}/${VERSION}/maven-metadata.xml"
 VT=$(wget --no-check-certificate -O- $URL | grep -m 1 \<value\> | sed -e 's/<value>\(.*\)<\/value>/\1/' | sed -e 's/ //g')
 
 URL="${PROTO}://${NEXUSREPO}/service/local/repositories/${REPO}/content/${GROUPID//.//}/${ARTIFACTID}/${VERSION}/${ARTIFACTID}-${VT}.jar"
-#wget --no-check-certificate "${URL}" -O "/opt/tca/${ARTIFACTID}-${VERSION%-SNAPSHOT}.jar"
-wget --no-check-certificate "${URL}" -O "${ARTIFACTID}-${VERSION%-SNAPSHOT}.jar"
+#wget --no-check-certificate "${URL}" -O "${ARTIFACTPATH}${ARTIFACTID}-${VERSION%-SNAPSHOT}.jar"
+wget --no-check-certificate "${URL}" -O "${ARTIFACTPATH}${ARTIFACTID}.${VERSION}.jar"
 
