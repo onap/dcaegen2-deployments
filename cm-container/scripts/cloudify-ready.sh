b903df7b8863e59db94d893b61d87619ebd1ce2b
Jack Lucas <jflucas@research.att.com>
2019-03-22 18:31:55 -0400

    Fix support for local type file storage
    Issue-ID: DCAEGEN2-989
    Change-Id: I9732fa0ff38affa320b6a872b74c16f87b73eb67
    Signed-off-by: Jack Lucas <jflucas@research.att.com>

---
+++ cm-container/scripts/cloudify-ready.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# ============LICENSE_START=======================================================
+# org.onap.dcae
+# ================================================================================
+# Copyright (c) 2019 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+#
+# Checking Cloudify Manager readiness by looking
+# for non-running services
+# Relying on the output format of the "cfy status" command.
+# A successful execution of the command outputs:
+#
+# cfy status
+# Retrieving manager services status... [ip=localhost]
+#
+# Services:
+# +--------------------------------+---------+
+# |            service             |  status |
+# +--------------------------------+---------+
+# | InfluxDB                       | running |
+# | Logstash                       | running |
+# | AMQP InfluxDB                  | running |
+# | RabbitMQ                       | running |
+# | Webserver                      | running |
+# | Management Worker              | running |
+# | PostgreSQL                     | running |
+# | Cloudify Console               | running |
+# | Manager Rest-Service           | running |
+# | Riemann                        | running |
+# +--------------------------------+---------+
+#
+# When an individual service is not running, it will have a status other than "running".
+# If the Cloudify API cannot be reached, the "Services:" line will not appear.
+
+STAT=$(cfy status)
+if (echo "${STAT}" | grep "^Services:$")
+then
+   echo "Got a status response"
+   if !(echo "${STAT}" | egrep '^\| [[:alnum:]]+'| grep -iv '| running ')
+   then
+      echo "All services running"
+      exit 0
+   else
+      echo "Some service(s) not running"
+   fi
+else
+  echo "Did not get a status response"
+fi
+echo "${STAT}"
+exit 1
--- cm-container/scripts/get-type-files.sh
+++ cm-container/scripts/get-type-files.sh
@@ -2,7 +2,7 @@
 # ============LICENSE_START=======================================================
 # org.onap.dcae
 # ================================================================================
-# Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+# Copyright (c) 2018-2019 AT&T Intellectual Property. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,6 +26,7 @@
 #
 set -x
 DEST=/opt/manager/resources/onapspec
+EXTRA_RULES=/opt/manager/extra-resolver-rules
 
 DCAETYPEFILES=\
 "\
@@ -60,11 +61,17 @@ done
 chown cfyuser:cfyuser ${DEST}
 
 # Add our local type file store to CM import resolver configuration
-TYPE_RULE0="{$1: file://${DEST}}"
-TYPE_RULE1="{$2: file://${DEST}}"
+TYPE_RULE0="{\"$1\": \"file://${DEST}\"}"
+TYPE_RULE1="{\"$2\": \"file://${DEST}\"}"
 # This sed re is 'brittle' but we can be sure the config.yaml file
 # won't change as long as we do not change the source Docker image for CM
 sed -i -e "s#      rules:#      rules:\n      - ${TYPE_RULE0}#" /etc/cloudify/config.yaml
 sed -i -e "s#      rules:#      rules:\n      - ${TYPE_RULE1}#" /etc/cloudify/config.yaml
 
 chown cfyuser:cfyuser /etc/cloudify/config.yaml
+
+# Changing /etc/cloudify/config.yaml is no longer sufficient
+# Need to provide the additional rules in a file that can be
+# used at deployment time to update the resolver rules
+echo "- ${TYPE_RULE0}" > ${EXTRA_RULES}
+echo "- ${TYPE_RULE1}" >> ${EXTRA_RULES}
