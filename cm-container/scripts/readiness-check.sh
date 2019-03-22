205fc2e449f9bcc2f447c58d2bd15e9b563237d3
Jack Lucas <jflucas@research.att.com>
2019-03-22 18:31:55 -0400

    Fix support for local type file storage
    Also move to script-based readiness check so that the import resolver
    rule updated needed for local type file storage takes effect before
    clients try to use CM, and to verify that all CM services are really up.
    
    Issue-ID: DCAEGEN2-989
    Change-Id: I9732fa0ff38affa320b6a872b74c16f87b73eb67
    Signed-off-by: Jack Lucas <jflucas@research.att.com>

---
+++ cm-container/scripts/readiness-check.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# ============LICENSE_START=======================================================
+# org.onap.dcae
+# ================================================================================
+# Copyright (c) 2019 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+#
+# Check whether Cloudify Manager is ready to take traffic
+# Two conditions must be met:
+#    -- The import resolver rules must have been updated.
+#       This is indicated by the presence of the file named
+#       /opt/manager/extra-resolver-rules-loaded.
+#    -- All Cloudify Manager services must be running, as
+#       indicated by the output of the cfy status command.
+
+RULES_LOADED=/opt/manager/extra-resolver-rules-loaded
+
+set -x
+
+if [[ -f  $RULES_LOADED ]]
+then
+  # Check for all services running
+  if /scripts/cloudify-ready.sh
+  then
+    exit 0
+  fi
+fi
+exit 1
