0440991c24d6fc5e2aaf0a5ae88747f5ce011830
Jack Lucas <jflucas@research.att.com>
2019-04-23 14:34:52 -0400

    Update k8s type file version
    Issue-ID: DCAEGEN2-1009
    Change-Id: I38ab743c0bf1c2a1116b6e50f494c38028fa9872
    Signed-off-by: Jack Lucas <jflucas@research.att.com>

--- cm-container/scripts/get-type-files.sh
+++ cm-container/scripts/get-type-files.sh
@@ -31,7 +31,7 @@ DCAETYPEFILES=\
 /dcaepolicyplugin/2.3.0/dcaepolicyplugin_types.yaml \
 /relationshipplugin/1.0.0/relationshipplugin_types.yaml \
 /k8splugin/1.4.5/k8splugin_types.yaml \
-/k8splugin/1.4.12/k8splugin_types.yaml \
+/k8splugin/1.4.13/k8splugin_types.yaml \
 
 "
 
