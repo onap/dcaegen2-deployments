3ed26737cd224861e36703df5c7b7bb3a9591220
Vijay Venkatesh Kumar <vv770d@att.com>
2019-05-06 16:56:23 +0000

    R5 tag update
    Change-Id: Iea3ba723cba257c02c2cfdfa04153a93b0c8c8a9
    Signed-off-by: Vijay Venkatesh Kumar <vv770d@att.com>
    Issue-ID: DCAEGEN2-1481

--- k8s-bootstrap-container/test-expand.sh
+++ k8s-bootstrap-container/test-expand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed \
-  -e 's#{{ ONAPTEMPLATE_RAWREPOURL_org_onap_dcaegen2_platform_plugins_releases }}#https://nexus.onap.org/service/local/repositories/raw/content/org.onap.dcaegen2.platform.plugins/R4#' \
+  -e 's#{{ ONAPTEMPLATE_RAWREPOURL_org_onap_dcaegen2_platform_plugins_releases }}#https://nexus.onap.org/service/local/repositories/raw/content/org.onap.dcaegen2.platform.plugins/R5#' \
   -e 's#{{ ONAPTEMPLATE_RAWREPOURL_org_onap_ccsdk_platform_plugins_releases }}#https://nexus.onap.org/service/local/repositories/raw/content/org.onap.ccsdk.platform.plugins#' \
-  -e 's#{{ ONAPTEMPLATE_RAWREPOURL_org_onap_dcaegen2_platform_blueprints_releases }}#https://nexus.onap.org/service/local/repositories/raw/content/org.onap.dcaegen2.platform.blueprints/R4#' \
+  -e 's#{{ ONAPTEMPLATE_RAWREPOURL_org_onap_dcaegen2_platform_blueprints_releases }}#https://nexus.onap.org/service/local/repositories/raw/content/org.onap.dcaegen2.platform.blueprints/R5#' \
 Dockerfile-template > Dockerfile
