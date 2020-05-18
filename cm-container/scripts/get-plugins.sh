aa048d658b7d919522f5664891fe4344d539c5e8
Jack Lucas <jflucas@research.att.com>
2020-05-18 18:23:38 -0400

    Move plugin upload to CM container
    Issue-ID: DCAEGEN2-2236
    Issue-ID: DCAEGEN2-1789
    Signed-off-by: Jack Lucas <jflucas@research.att.com>
    Change-Id: Ibc4612dedbf73a17a237b88abe198add391b82de

---
+++ cm-container/scripts/get-plugins.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# ============LICENSE_START=======================================================
+# Copyright (c) 2018-2020 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+
+# Pull plugin wagon files and type files from repo
+# $1 is the DCAE repo URL - script assumes all files come from the
+#    same repo, but necessarily same paths
+#
+set -x -e
+
+# Location in CM container where plugins/type files will be stored
+# At deployment, CM script will look here to find plugins to upload
+DEST=${DEST:-/opt/plugins}
+
+# Each line has a plugin wagon/type file pair, in the form
+# /path/to/plugin/wagon|/path/to/type/file
+PLUGINS=\
+"\
+/dcaepolicyplugin/2.4.0/dcaepolicyplugin-2.4.0-py27-none-linux_x86_64.wgn|/dcaepolicyplugin/2.4.0/dcaepolicyplugin_types.yaml \
+/relationshipplugin/1.1.0/relationshipplugin-1.1.0-py27-none-linux_x86_64.wgn|/relationshipplugin/1.1.0/relationshipplugin_types.yaml \
+/k8splugin/2.0.0/k8splugin-2.0.0-py27-none-linux_x86_64.wgn|/k8splugin/2.0.0/k8splugin_types.yaml \
+/k8splugin/3.0.0/k8splugin-3.0.0-py27-none-linux_x86_64.wgn|/k8splugin/3.0.0/k8splugin_types.yaml \
+/clamppolicyplugin/1.1.0/clamppolicyplugin-1.1.0-py27-none-linux_x86_64.wgn|/clamppolicyplugin/1.1.0/clamppolicyplugin_types.yaml
+"
+
+mkdir -p ${DEST}
+
+for p in ${PLUGINS}
+do
+  w=$(echo $p | cut -d '|' -f1)
+  t=$(echo $p | cut -d '|' -f2)
+
+  # Put each wagon/type file pair into its own subdirectory
+  # This prevents name collisions which can happen because
+  # type files don't embed a version.
+	subdir=$(mktemp -d -t plugin-XXXXXXX --tmpdir=${DEST})
+
+	curl -Ss -L -f $1/$t >> ${subdir}/$(basename $t)
+  curl -Ss -L -f $1/$w >> ${subdir}/$(basename $w)
+
+done
+
+chown -R cfyuser:cfyuser ${DEST}
---
+++ cm-container/scripts/load-plugins.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+# ============LICENSE_START=======================================================
+# Copyright (c) 2019-2020 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+# Runs at deployment time to load the plugins/type files
+# that were stored into the container file system at build time
+
+PLUGINS_LOADED=/opt/manager/plugins-loaded
+PLUGIN_DIR=/opt/plugins
+
+# Set defaults for CM address, protocol, and port
+CMADDR=${CMADDR:-dcae-cloudify-manager}
+CMPROTO=${CMPROTO:-https}
+CMPORT=${CMPORT:-443}
+
+# Password is currently fixed at the default
+# Eventually the password will be passed in as an environment variable
+CMPASS=${CMPASS:-admin}
+
+# Set up additional parameters for using HTTPS
+CACERT="/opt/onap/certs/cacert.pem"
+CURLTLS=""
+if [ $CMPROTO = "https" ]
+then
+    CURLTLS="--cacert $CACERT"
+fi
+
+### FUNCTION DEFINITIONS ###
+
+# cm_hasany: Query Cloudify Manager and return 0 (true) if there are any entities matching the query
+# Used to see if something is already present on CM
+# $1 -- query fragment, for instance "plugins?archive_name=xyz.wgn" to get
+#  the number of plugins that came from the archive file "xyz.wgn"
+function cm_hasany {
+    # We use _include=id to limit the amount of data the CM sends back
+    # We rely on the "metadata.pagination.total" field in the response
+    # for the total number of matching entities
+    COUNT=$(curl -Ss -H "Tenant: default_tenant" --user admin:${CMPASS} ${CURLTLS} "${CMPROTO}://${CMADDR}:${CMPORT}/api/v3.1/$1&_include=id" \
+             | /bin/jq .metadata.pagination.total)
+    if (( $COUNT > 0 ))
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Install plugin if it's not already installed
+# $1 -- path to wagon file for plugin
+# $2 -- path to type file for plugin
+function install_plugin {
+    ARCHIVE=$(basename $1)
+    # See if it's already installed
+    if cm_hasany "plugins?archive_name=$ARCHIVE"
+    then
+        echo plugin $1 already installed on ${CMADDR}
+    else
+        cfy plugin upload  -y $2 $1
+    fi
+}
+
+### END FUNCTION DEFINTIONS ###
+
+set -ex
+
+
+# Wait for Cloudify Manager to come up
+while ! /scripts/cloudify-ready.sh
+do
+    echo "Waiting for CM to come up"
+    sleep 15
+done
+
+if [[ ! -f ${PLUGINS_LOADED} ]]
+then
+
+  # Each subdirectory of ${PLUGIN_DIR} contains a wagon (.wgn) and type file (.yaml)
+  for p in ${PLUGIN_DIR}/*
+  do
+    # Expecting exactly 1 .wgn and 1 .yaml
+    # But just in case, taking only the first of each
+    # (If either is missing, will fail on install_plugin)
+    wagons=($p/*.wgn)
+    types=($p/*.yaml)
+    install_plugin ${wagons[0]} ${types[0]}
+  done
+
+  touch ${PLUGINS_LOADED}
+else
+  echo "Plugins already loaded"
+fi
