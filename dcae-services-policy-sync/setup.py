40511bdccfd7a913ccd9628f2a8a950f0a47288b
Vijay Venkatesh Kumar <vv770d@att.com>
2021-04-16 19:54:15 +0000

    Fix cert issue on policy-sync
    Change-Id: I0396fc49a486d32d1e063ad8f2ff8dba9d51b96f
    Signed-off-by: Vijay Venkatesh Kumar <vv770d@att.com>
    Issue-ID: DCAEGEN2-2735
    Signed-off-by: Vijay Venkatesh Kumar <vv770d@att.com>

--- dcae-services-policy-sync/setup.py
+++ dcae-services-policy-sync/setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 setup(
     name="policysync",
     author="Chris Luckenbaugh",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["aiohttp>=2.3", "PyYAML", "prometheus_client"],
